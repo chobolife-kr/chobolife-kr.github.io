96b2a1a73653340924e99734fc198a30e417d901
chobolife-kr <k30626@gmail.com>
2025-11-30 23:39:46 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Chobolife",
+    pageTitle: "주제가 없는 블로그",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
